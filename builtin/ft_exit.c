254a6f7ad02913ae0f200ff3105b8555135a1485
Andrey Arama <anarama@c3r2p5.42vienna.com>
2024-08-12 12:09:51 +0200

    improved ft_exit

--- builtin/ft_exit.c
+++ builtin/ft_exit.c
@@ -6,11 +6,25 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 16:52:07 by vvobis            #+#    #+#             */
-/*   Updated: 2024/08/05 17:29:03 by anarama          ###   ########.fr       */
+/*   Updated: 2024/08/12 11:59:55 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+static int	check_input(char *str, int num)
+{
+	char	*check;
+
+	check = ft_itoa(num);
+	if (!check || ft_strncmp(check, str, ft_strlen(str)) != 0)
+	{
+		free(check);
+		return (0);
+	}
+	free(check);
+	return (1);
+}
 
 void	ft_exit(char **args, int *exit_status)
 {
@@ -21,11 +35,17 @@ void	ft_exit(char **args, int *exit_status)
 	if (arg_count > 2)
 	{
 		ft_printf("minishell: exit: too many arguments\n");
-		*exit_status = 1;	
+		*exit_status = 1;
+		return ;
 	}
 	else if (arg_count == 2)
 	{
-		*exit_status = ft_atoi(args[1]); // handle intmax intmin
+		*exit_status = ft_atoi(args[1]);
+		if (!check_input(args[1], *exit_status))
+		{
+			ft_printf("minishell: exit: %s: numeric argument required\n", args[1]);
+			*exit_status = 2;
+		}
 	}
 	else
 		*exit_status = 0;
